72c1ec2f812e5fd02958d81ea65381dc465bbc44
Wagner Quiros <wagner@biocenosismarina.org>
2016-05-04 11:07:14 -0700

    Wagner try

---
+++ data/CorvinaSizesHistogram.R
@@ -0,0 +1,26 @@
+#Wagner Quiros.
+#CorvinaReina histogram. (The legal mesh size is 3")
+#Year After closure 2009. Mesh size 2.5". Illegal. n=437
+CorvinaReinaSizes2009 <- read.csv("~/Desktop/Group Project NICOYAFisheries/nicoyafisheries.github.io/data/CorvinaReinaSizes2009.csv")
+View(CorvinaReinaSizes2009)
+hist(CorvinaReinaSizes2009$Sizes)
+
+
+############################################
+#CorvinaReina histogram
+#Year After closure 2012. Mesh size 2.5" . Illegal.n=548
+CorvinaReinaSizes2012. <- read.csv("~/Desktop/Group Project NICOYAFisheries/nicoyafisheries.github.io/data/CorvinaReinaSizes2012..csv")
+View(CorvinaReinaSizes2012.)
+hist(CorvinaReinaSizes2012.$Sizes)
+
+
+#############################################
+#CorvinaReina histogram
+#Year After closure 2014. Mesh size 2.5", 3" and 3.5". Illegal.n=516.
+CorvinaReinaSizes2014. <- read.csv("~/Desktop/Group Project NICOYAFisheries/nicoyafisheries.github.io/data/CorvinaReinaSizes2014..csv")
+View(CorvinaReinaSizes2014.)
+hist(CorvinaReinaSizes2014.$Size)
+
+
+#############################################
+
