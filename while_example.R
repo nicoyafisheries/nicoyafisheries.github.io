8a72f395fddcfdf001b3b98a0f1df42e67cc26cc
alxsmith <aedwards.smith@gmail.com>
2016-11-21 13:22:41 -0800

    ignore this

---
+++ while_example.R
@@ -0,0 +1,16 @@
+a = 1
+
+for 2:100
+
+
+i = t
+while (continue == TRUE){
+  run your fisheries model
+  
+  
+  t = t+1
+  
+  if (  sum(N_at[t,]-N_at[t-1,])<.001) {
+    continue = FALSE
+  } else( continue = TRUE)
+}
