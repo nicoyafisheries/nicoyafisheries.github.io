1f0e1d744521133c9cb88fd1848cbc81673aa654
alxsmith <aedwards.smith@gmail.com>
2017-03-10 08:53:35 -0800

    updated scripts for analysis

--- Functions/MyTheme.R
+++ Functions/MyTheme.R
@@ -3,7 +3,6 @@ MyTheme = function(plot){
   
   
   plot +
-  theme_minimal() +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank() ,axis.line = element_line(colour = "black")) +
    theme(legend.text = element_text(size = 12), 
             legend.title = element_text(size = 13))+
