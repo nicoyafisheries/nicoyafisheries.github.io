a1266560c3541d5017c49c56c85a459da2458ff4
alxsmith <aedwards.smith@gmail.com>
2017-04-18 00:15:36 -0700

    theme makes lengend text bigger

--- Functions/MyTheme.R
+++ Functions/MyTheme.R
@@ -4,12 +4,15 @@ MyTheme = function(plot){
   
   plot +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank() ,axis.line = element_line(colour = "black")) +
-   theme(legend.text = element_text(size = 12), 
-            legend.title = element_text(size = 13))+
+   theme(legend.text = element_text(size = 15), 
+            legend.title = element_text(size = 16))+
     theme(axis.text.x = element_text(size=17),
           axis.text.y = element_text(size=17),
           axis.title.x = element_text(size=18),
-          axis.title.y = element_text(size=18))
+          axis.title.y = element_text(size=18))+
+     theme(axis.ticks = element_line(colour = "black"), 
+            axis.text = element_text(colour = "black"))
+  
   
   
   
